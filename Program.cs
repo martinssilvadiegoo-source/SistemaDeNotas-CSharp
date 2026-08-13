8759a6b840e3978dbadf299e1008c867e7dd2017
martinssilvadiegoo-source <martins.silva.diegoo@gmail.com>
2026-08-13 17:15:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Sistema de notas");
+
+Console.WriteLine("Digite nome do aluno");
+
+string? nomeAluno = Console.ReadLine();
+
+Console.WriteLine("Qual sua nota?");
+
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Qual sua segunda nota?");
+
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+double media = (nota1 + nota2) / 2;
+
+Console.WriteLine("Media: " + media);
+
+if (media >= 7)
+{
+    Console.WriteLine("Situação: Aprovado");
+}
+else
+{
+    Console.WriteLine("Situação: Reprovado");
+}
